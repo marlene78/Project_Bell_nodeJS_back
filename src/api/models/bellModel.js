be786c0373a4c7406e9fa470a7db5d47ceb5a6c7
marlene <marlene.lingisi@yahoo.fr>
2020-03-14 17:03:16 +0100

    modification model

--- src/api/models/bellModel.js
+++ src/api/models/bellModel.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let bellSchema = new Schema({
-  name: String,
-  lien: String
+  name: {
+    type: String,
+    required: "Le titre est requis"
+  },
+  lien: {
+    type: String,
+    required: "Le lien est requis"
+  }
 });
 
 module.exports = mongoose.model('Bell', bellSchema);
