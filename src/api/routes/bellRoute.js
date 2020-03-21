b9d22e9072744c6c02c292c411ce02aea9a45bb2
anta <antandoye0@gmail.com>
2020-03-21 18:55:39 +0100

    issue-add-vote

--- src/api/routes/bellRoute.js
+++ src/api/routes/bellRoute.js
@@ -4,9 +4,12 @@ module.exports = (server) => {
   
     server.route('/bell')
     .get(bellController.list_all_bell)
-    .post(bellController.create_a_bell);
+    .post(bellController.create_a_bell)
+    
   
     server.route('/bells/:bell_id') // req.params.bell_id
     .get(bellController.get_a_bell) // affiche une sonnerie 
+    .put(bellController.update_a_bell)
+    
   }
   
